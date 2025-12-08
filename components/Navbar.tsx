bb0ed51b5f814c9fa5a4dce52a6df87f60d0f478
TJ745 <talha_mughalz@hotmail.com>
2025-12-08 12:25:49 +0300

    Finish Welcome

---
+++ components/Navbar.tsx
@@ -0,0 +1,40 @@
+import dayjs from "dayjs";
+import Image from "next/image";
+import { navIcons, navLinks } from "@/constants";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <Image src="/images/logo.svg" alt="navbar-bg" width={10} height={10} />
+        <p className="font-bold">TJ&apos;s Portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <Image
+                src={img}
+                alt={`icon-${id}`}
+                width={20}
+                height={20}
+                className="icon-hover"
+              />
+            </li>
+          ))}
+        </ul>
+
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
